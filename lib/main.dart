7d6442bf87853fb24083a44d5f0ae7aa011b3e71
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-11-14 19:08:55 -0500

    main.dart: develop increment and decrement app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FirstFlightApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstFlightApp extends StatelessWidget {
+  const FirstFlightApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,68 +15,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
-
-      home: const MyHomePage(title: 'First Flight Home Page'),
+      home: const CounterPage(title: 'First Flight Home Page'),
     );
   }
 }
 
-
-// Main page
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+// Second page
+class CounterPage extends StatefulWidget {
   final String title;
+  const CounterPage({super.key, required this.title});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CounterPage> createState() => _CounterPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+
+class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {_counter++;});
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}
-
-
-// Second page
-class SecondPage extends StatefulWidget {
-  final String title;
-  const SecondPage({super.key, required this.title});
-
-  @override
-  State<SecondPage> createState() => _SecondPageState();
-}
-
-
-class _SecondPageState extends State<SecondPage> {
-  int _counter = 100;
 
   void _decrementCounter() {
     setState(() {_counter--;});
@@ -89,21 +48,31 @@ class _SecondPageState extends State<SecondPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('The current value is'),
             Text('$_counter', style: Theme.of(context).textTheme.headlineMedium,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.exposure_minus_1),
+                ),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.exposure_plus_1),
+                ), 
+              ]
+            ),
           ],
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _decrementCounter,
-        tooltip: 'Decrement',
-        child: const Icon(Icons.exposure_minus_1),
-      ),
+      ), 
     );
   }
 }
