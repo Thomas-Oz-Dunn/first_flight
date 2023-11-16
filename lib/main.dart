b9966da8f5c2aa8cf7a80c64225927acefb310c5
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-11-16 14:47:23 -0500

    main.dart: investigate camera handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:image_picker/image_picker.dart';
 
 var indicatorColor = Colors.blue[800];
 
@@ -67,6 +68,11 @@ class _MainPageState extends State<MainPage> {
           icon: Icon(Icons.abc_outlined),
           label: 'Text',
         ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.photo), 
+          icon: Icon(Icons.photo_outlined), 
+          label: 'Picture'
+        ),
       ];
       
     var pages = <Widget>[
@@ -76,6 +82,7 @@ class _MainPageState extends State<MainPage> {
           child: const Text('Home Page'),
         ),
         const TextPage(),
+        const ImagePickerPage(),
       ];
     
     var mainPageLayout = Scaffold(
@@ -237,3 +244,227 @@ class _CounterPageState extends State<CounterPage> {
     return pageBody;
   }
 }
+
+
+class ImagePickerPage extends StatefulWidget {
+  const ImagePickerPage({super.key});
+
+  @override
+  State<ImagePickerPage> createState() => _ImagePickerState();
+}
+
+class _ImagePickerState extends State<ImagePickerPage> {
+  final ImagePicker _picker = ImagePicker();
+  
+  List<XFile>? _mediaFileList;
+  
+  void _setImageFileListFromFile(XFile? value) {
+    _mediaFileList = value == null ? null : <XFile>[value];
+  }
+
+  dynamic _pickImageError;
+  String? _retrieveDataError;
+
+  final TextEditingController maxWidthController = TextEditingController();
+  final TextEditingController maxHeightController = TextEditingController();
+  final TextEditingController qualityController = TextEditingController();
+      
+  Future<void> _displayPickImageDialog(
+        BuildContext context, 
+        OnPickImageCallback onPick
+      ) async {
+      return showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Add optional parameters'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    controller: maxWidthController,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    decoration: const InputDecoration(
+                        hintText: 'Enter maxWidth if desired'),
+                  ),
+                  TextField(
+                    controller: maxHeightController,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    decoration: const InputDecoration(
+                        hintText: 'Enter maxHeight if desired'),
+                  ),
+                  TextField(
+                    controller: qualityController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter quality if desired'),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                    child: const Text('PICK'),
+                    onPressed: () {
+                      final double? width = maxWidthController.text.isNotEmpty
+                          ? double.parse(maxWidthController.text)
+                          : null;
+                      final double? height = maxHeightController.text.isNotEmpty
+                          ? double.parse(maxHeightController.text)
+                          : null;
+                      final int? quality = qualityController.text.isNotEmpty
+                          ? int.parse(qualityController.text)
+                          : null;
+                      onPick(width, height, quality);
+                      Navigator.of(context).pop();
+                    }),
+              ],
+            );
+          });
+    }
+
+  Future<void> _onImageButtonPressed(
+    ImageSource source, {
+    required BuildContext context,
+    bool isMultiImage = false,
+    bool isMedia = false,
+  }) async {
+    if (context.mounted) {
+      if (isMultiImage) {
+        await _displayPickImageDialog(
+          context,
+            (double? maxWidth, double? maxHeight, int? quality) async {
+          try {
+            final List<XFile> pickedFileList = isMedia
+                ? await _picker.pickMultipleMedia(
+                    maxWidth: maxWidth,
+                    maxHeight: maxHeight,
+                    imageQuality: quality,
+                  )
+                : await _picker.pickMultiImage(
+                    maxWidth: maxWidth,
+                    maxHeight: maxHeight,
+                    imageQuality: quality,
+                  );
+            setState(() {
+              _mediaFileList = pickedFileList;
+            });
+          } catch (e) {
+            setState(() {
+              _pickImageError = e;
+            });
+          }
+        });
+      } else if (isMedia) {
+        await _displayPickImageDialog(context,
+            (double? maxWidth, double? maxHeight, int? quality) async {
+          try {
+            final List<XFile> pickedFileList = <XFile>[];
+            final XFile? media = await _picker.pickMedia(
+              maxWidth: maxWidth,
+              maxHeight: maxHeight,
+              imageQuality: quality,
+            );
+            if (media != null) {
+              pickedFileList.add(media);
+              setState(() {
+                _mediaFileList = pickedFileList;
+              });
+            }
+          } catch (e) {
+            setState(() {
+              _pickImageError = e;
+            });
+          }
+        });
+      } else {
+        await _displayPickImageDialog(context,
+            (double? maxWidth, double? maxHeight, int? quality) async {
+          try {
+            final XFile? pickedFile = await _picker.pickImage(
+              source: source,
+              maxWidth: maxWidth,
+              maxHeight: maxHeight,
+              imageQuality: quality,
+            );
+            setState(() {
+              _setImageFileListFromFile(pickedFile);
+            });
+          } catch (e) {
+            setState(() {
+              _pickImageError = e;
+            });
+          }
+        });
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    maxWidthController.dispose();
+    maxHeightController.dispose();
+    qualityController.dispose();
+    super.dispose();
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      if (response.type == RetrieveType.image) {
+        setState(() {
+          if (response.files == null) {
+            _setImageFileListFromFile(response.file);
+          } else {
+            _mediaFileList = response.files;
+          }
+        });
+      }
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    var pageBody = Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          if (_picker.supportsImageSource(ImageSource.camera))
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: FloatingActionButton(
+                onPressed: () {
+                  _onImageButtonPressed(
+                    ImageSource.camera, 
+                    context: context
+                  );
+                },
+                heroTag: 'image2',
+                tooltip: 'Take a Photo',
+                child: const Icon(Icons.camera_alt),
+              ),
+            ),
+        ],
+      ),
+    );
+
+    return pageBody;
+  }
+}
+
+typedef OnPickImageCallback = void Function(
+    double? maxWidth, 
+    double? maxHeight, 
+    int? quality
+);
