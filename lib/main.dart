0d0d6e91f8b257c1b69816ad5a6fb44ae0875e9c
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-11-19 09:58:52 -0500

    main.dart: clean up request page interface

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:image_picker/image_picker.dart';
 
 // Internal imports
 // import 'package:first_flight/vizPage.dart'
 
-var indicatorColor = Colors.blue[800];
-// Define color set
-
+const double objSpacing = 10;
+const indicatorColor =  Color.fromARGB(255, 15, 83, 157);
+const buttonColor = Color.fromARGB(255, 15, 134, 122);
 
 // Goals of the app
 // Home Page
@@ -50,8 +49,8 @@ class FirstFlightApp extends StatelessWidget {
       title: 'First Flight',
       theme: ThemeData(
         useMaterial3: true,
-        primaryColor: Colors.blue[500],
-        hoverColor: Colors.blue[100]
+        primaryColor: indicatorColor,
+        hoverColor: buttonColor
       ),
       home: const MainPage(),
     );
@@ -94,11 +93,6 @@ class _MainPageState extends State<MainPage> {
           label: 'Home',
         ),
         NavigationDestination(
-          selectedIcon: Icon(Icons.abc),
-          icon: Icon(Icons.abc_outlined),
-          label: 'Text',
-        ),
-        NavigationDestination(
           selectedIcon: Icon(Icons.textsms), 
           icon: Icon(Icons.textsms_outlined), 
           label: 'Feature Request'
@@ -111,7 +105,6 @@ class _MainPageState extends State<MainPage> {
           alignment: Alignment.center,
           child: const Text('Home Page'),
         ),
-        const TextPage(),
         const RequestFeaturePage(),
       ];
     
@@ -126,35 +119,6 @@ class _MainPageState extends State<MainPage> {
     );
 
     return mainPageLayout;
-  }
-}
-
-// Text Field Page
-class TextPage extends StatelessWidget {
-  const TextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const searchBox = TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: 'Enter a search term',
-      ),
-    );
-
-    return const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 32, 
-              vertical: 32
-            ),
-            child: searchBox
-          ),
-        ],
-    );
   }
 }
 
@@ -227,7 +191,7 @@ class _CounterPageState extends State<CounterPage> {
         backgroundColor: Colors.red,
         child: const Icon(Icons.exposure_minus_1),
       ),
-      const SizedBox(width: 20),
+      const SizedBox(width: objSpacing),
       FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
@@ -264,7 +228,7 @@ class _CounterPageState extends State<CounterPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: buttons
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: objSpacing),
             resetButton,
           ],
         ),
@@ -274,231 +238,6 @@ class _CounterPageState extends State<CounterPage> {
     return pageBody;
   }
 }
-
-
-class ImagePickerPage extends StatefulWidget {
-  const ImagePickerPage({super.key});
-
-  @override
-  State<ImagePickerPage> createState() => _ImagePickerState();
-}
-
-class _ImagePickerState extends State<ImagePickerPage> {
-  final ImagePicker _picker = ImagePicker();
-  
-  List<XFile>? _mediaFileList;
-  
-  void _setImageFileListFromFile(XFile? value) {
-    _mediaFileList = value == null ? null : <XFile>[value];
-  }
-
-  dynamic _pickImageError;
-  String? _retrieveDataError;
-
-  final TextEditingController maxWidthController = TextEditingController();
-  final TextEditingController maxHeightController = TextEditingController();
-  final TextEditingController qualityController = TextEditingController();
-      
-  Future<void> _displayPickImageDialog(
-        BuildContext context, 
-        OnPickImageCallback onPick
-      ) async {
-      return showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Add optional parameters'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    controller: maxWidthController,
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    decoration: const InputDecoration(
-                        hintText: 'Enter maxWidth if desired'),
-                  ),
-                  TextField(
-                    controller: maxHeightController,
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    decoration: const InputDecoration(
-                        hintText: 'Enter maxHeight if desired'),
-                  ),
-                  TextField(
-                    controller: qualityController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        hintText: 'Enter quality if desired'),
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                    child: const Text('PICK'),
-                    onPressed: () {
-                      final double? width = maxWidthController.text.isNotEmpty
-                          ? double.parse(maxWidthController.text)
-                          : null;
-                      final double? height = maxHeightController.text.isNotEmpty
-                          ? double.parse(maxHeightController.text)
-                          : null;
-                      final int? quality = qualityController.text.isNotEmpty
-                          ? int.parse(qualityController.text)
-                          : null;
-                      onPick(width, height, quality);
-                      Navigator.of(context).pop();
-                    }),
-              ],
-            );
-          });
-    }
-
-  Future<void> _onImageButtonPressed(
-    ImageSource source, {
-    required BuildContext context,
-    bool isMultiImage = false,
-    bool isMedia = false,
-  }) async {
-    if (context.mounted) {
-      if (isMultiImage) {
-        await _displayPickImageDialog(
-          context,
-            (double? maxWidth, double? maxHeight, int? quality) async {
-          try {
-            final List<XFile> pickedFileList = isMedia
-                ? await _picker.pickMultipleMedia(
-                    maxWidth: maxWidth,
-                    maxHeight: maxHeight,
-                    imageQuality: quality,
-                  )
-                : await _picker.pickMultiImage(
-                    maxWidth: maxWidth,
-                    maxHeight: maxHeight,
-                    imageQuality: quality,
-                  );
-            setState(() {
-              _mediaFileList = pickedFileList;
-            });
-          } catch (e) {
-            setState(() {
-              _pickImageError = e;
-            });
-          }
-        });
-      } else if (isMedia) {
-        await _displayPickImageDialog(context,
-            (double? maxWidth, double? maxHeight, int? quality) async {
-          try {
-            final List<XFile> pickedFileList = <XFile>[];
-            final XFile? media = await _picker.pickMedia(
-              maxWidth: maxWidth,
-              maxHeight: maxHeight,
-              imageQuality: quality,
-            );
-            if (media != null) {
-              pickedFileList.add(media);
-              setState(() {
-                _mediaFileList = pickedFileList;
-              });
-            }
-          } catch (e) {
-            setState(() {
-              _pickImageError = e;
-            });
-          }
-        });
-      } else {
-        await _displayPickImageDialog(context,
-            (double? maxWidth, double? maxHeight, int? quality) async {
-          try {
-            final XFile? pickedFile = await _picker.pickImage(
-              source: source,
-              maxWidth: maxWidth,
-              maxHeight: maxHeight,
-              imageQuality: quality,
-            );
-            setState(() {
-              _setImageFileListFromFile(pickedFile);
-            });
-          } catch (e) {
-            setState(() {
-              _pickImageError = e;
-            });
-          }
-        });
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    maxWidthController.dispose();
-    maxHeightController.dispose();
-    qualityController.dispose();
-    super.dispose();
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostDataResponse response = await _picker.retrieveLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      if (response.type == RetrieveType.image) {
-        setState(() {
-          if (response.files == null) {
-            _setImageFileListFromFile(response.file);
-          } else {
-            _mediaFileList = response.files;
-          }
-        });
-      }
-    } else {
-      _retrieveDataError = response.exception!.code;
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    var pageBody = Scaffold(
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          if (_picker.supportsImageSource(ImageSource.camera))
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: FloatingActionButton(
-                onPressed: () {
-                  _onImageButtonPressed(
-                    ImageSource.camera, 
-                    context: context
-                  );
-                },
-                heroTag: 'image2',
-                tooltip: 'Take a Photo',
-                child: const Icon(Icons.camera_alt),
-              ),
-            ),
-        ],
-      ),
-    );
-
-    return pageBody;
-  }
-}
-
-typedef OnPickImageCallback = void Function(
-    double? maxWidth, 
-    double? maxHeight, 
-    int? quality
-);
-
 
 class RequestFeaturePage extends StatefulWidget{
 
@@ -553,32 +292,48 @@ class _RequestFeatureState extends State<RequestFeaturePage> {
   @override
   Widget build(BuildContext context) {
     var widgets = <Widget>[
-      TextField(
-        controller: controller,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          hintText: 'Enter a request',
-          ),
+      Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 2 * objSpacing, 
+          vertical: objSpacing
         ),
+        child: TextField(
+          maxLines: 10,
+          controller: controller,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter a request',
+            ),
+          ),
+      ),
+      const SizedBox(height: objSpacing),
       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FloatingActionButton(
             onPressed: _loadRequest,
-            tooltip: 'Load Request',
-            backgroundColor: Colors.blue,
-            child: const Icon(Icons.file_copy),
+            tooltip: 'Load',
+            backgroundColor: buttonColor,
+            child: const Icon(
+              Icons.file_copy,
+              color: Color.fromARGB(185, 255, 255, 255),
+              ),
           ),
+          const SizedBox(width: objSpacing),
           FloatingActionButton(
             onPressed: _saveRequest,
-            tooltip: 'Save Request',
-            backgroundColor: Colors.blue,
-            child: const Icon(Icons.save),
+            tooltip: 'Save',
+            backgroundColor: buttonColor,
+            child: const Icon(Icons.save,
+              color: Color.fromARGB(185, 255, 255, 255)),
           ),
+          const SizedBox(width: objSpacing),
           FloatingActionButton(
             onPressed: _clearRequest,
-            tooltip: 'Clear Request',
-            backgroundColor: Colors.blue,
-            child: const Icon(Icons.clear),
+            tooltip: 'Clear',
+            backgroundColor: buttonColor,
+            child: const Icon(Icons.clear, 
+              color: Color.fromARGB(185, 255, 255, 255)),
           ),
        ],
       )
@@ -593,7 +348,7 @@ class _RequestFeatureState extends State<RequestFeaturePage> {
     return pageLayout;
     
   }
-  // Clear button
+
   // Submit button
   // - Post to Issue page on GitHub or email
 }
