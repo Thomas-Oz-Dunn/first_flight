84b0536e07a37899b474d4bd5c0f8eefd7c38522
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-03 18:05:14 -0500

    main.dart: migrate to standalone files per page

--- lib/style.dart
+++ lib/style.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const double objSpacing = 10;
+
 const teal =  Color.fromARGB(255, 15, 83, 157);
 const blue = Color.fromARGB(255, 15, 134, 122);
 const white = Color.fromARGB(185, 255, 255, 255);
