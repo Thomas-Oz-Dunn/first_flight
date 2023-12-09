7f47ed10fe4585f9345fbca36ee3bd906bd582a8
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-08 20:37:50 -0500

    main.dart: run pre commit hook after push

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,11 @@ class _MainPageState extends State<MainPage> {
                   Icons.star,
                 ),
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()),);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingsPage()),
+                  );
                 },
               ),
               title: const Text('Home'),
