90c85ac533a285177394521cdc0b87da3d4cfb32
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-20 11:56:18 -0500

    all: clean slate protocol

--- lib/favorites_page.dart
+++ lib/favorites_page.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:first_flight/style.dart';
-
-// Favorites page
 class FavoritesPage extends StatefulWidget {
+// Favorites page 
+    // + button in bottom right to add new
+      // Pop up window to enter fields
+
+    // Search bar
+    // scroll
+      // each list member
+        // Title
+        // Metadata (next time)
+        // triple dot
+          // share
+          // delete
+        // single tap load trajector in main
+        // hold click to reorder
   const FavoritesPage({super.key});
 
   @override
-  State<FavoritesPage> createState() => _FavoritesState();
+  State<FavoritesPage> createState() => _FavoritesPageState();
 }
 
-class _FavoritesState extends State<FavoritesPage> {
+class _FavoritesPageState extends State<FavoritesPage> {
   SharedPreferences? preferences;
-  List<String> favorites = ['First'];
+  List<String> _allFavoritesList = [];
+  List<String> _filteredFavoritesList = [];
+
+  final TextEditingController _searchController = TextEditingController();
+  final _newFavoriteFieldController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  
+
+  void loadFavorites() {
+    List<String>? savedData = preferences?.getStringList('Favorites');
+
+    if (savedData != null) {
+      _allFavoritesList = savedData;
+      _filteredFavoritesList = _allFavoritesList;
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _filterListBySearchText(String searchText) {
+    setState(() {
+      _filteredFavoritesList = _allFavoritesList
+          .where((faveObj) =>
+              faveObj.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
 
   Future<void> initStorage() async {
     preferences = await SharedPreferences.getInstance();
@@ -23,12 +65,13 @@ class _FavoritesState extends State<FavoritesPage> {
   @override
   void initState() {
     initStorage();
+    loadFavorites();
     super.initState();
   }
 
   void _addFavorite(name) {
-    favorites.add(name);
-    preferences?.setStringList("Favorites", favorites);
+    _allFavoritesList.add(name);
+    preferences?.setStringList("Favorites", _allFavoritesList);
     setState(() {});
   }
 
@@ -36,51 +79,27 @@ class _FavoritesState extends State<FavoritesPage> {
     List<String>? savedData = preferences?.getStringList('Favorites');
 
     if (savedData == null) {
-      preferences?.setStringList("Favorites", favorites);
+      preferences?.setStringList("Favorites", _allFavoritesList);
     } else {
-      favorites = savedData;
+      _allFavoritesList = savedData;
+      _filteredFavoritesList = _allFavoritesList;
     }
 
     setState(() {});
   }
 
   void _removeFavorite(name) {
-    favorites.remove(name);
-    preferences?.setStringList("Favorites", favorites);
+    _allFavoritesList.remove(name);
+    _filteredFavoritesList.remove(name);
+    preferences?.setStringList("Favorites", _allFavoritesList);
     setState(() {});
   }
 
-  Widget _buildList() {
-    _loadFavorites();
-    var listBuilder = ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, itemIdxs) {
-        if (itemIdxs < favorites.length) {
-          var favoriteTiles = ListTile(
-              title: Text(favorites[itemIdxs],
-                  style: const TextStyle(fontSize: 18.0, color: white)),
-              trailing: IconButton(
-                icon: const Icon(
-                  Icons.delete,
-                ),
-                onPressed: () async {
-                  _removeFavorite(favorites[itemIdxs]);
-                },
-              ));
-          return favoriteTiles;
-        }
-      },
-    );
-    return listBuilder;
-  }
-
-  final _textFieldController = TextEditingController();
-
   Future<String?> _showTextInputDialog(BuildContext context) async {
     var dialogBox = AlertDialog(
-      title: const Text('Add new favorite', style: TextStyle(color: white)),
+      title: const Text('Add new favorite'),
       content: TextField(
-        controller: _textFieldController,
+        controller: _newFavoriteFieldController,
         decoration: const InputDecoration(hintText: "New Favorite"),
       ),
       actions: <Widget>[
@@ -90,7 +109,7 @@ class _FavoritesState extends State<FavoritesPage> {
         ),
         ElevatedButton(
           child: const Text('Add'),
-          onPressed: () => Navigator.pop(context, _textFieldController.text),
+          onPressed: () => Navigator.pop(context, _newFavoriteFieldController.text),
         ),
       ],
     );
@@ -102,30 +121,88 @@ class _FavoritesState extends State<FavoritesPage> {
         });
   }
 
+
   @override
   Widget build(BuildContext context) {
+
+    // TODO-TD: hide search bar unless scrolled up?
+    var favoritesSearchBar = AppBar(
+      leading: null,
+      title: Container(
+        height: 40,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+        child: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            prefixIcon: IconButton(
+              icon: const Icon(Icons.search_rounded),
+              onPressed: () => FocusScope.of(context).unfocus(),
+            ),
+            hintText: 'Search Favorites',
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.clear_rounded),
+              onPressed: () {
+                _searchController.text = "";
+                _filterListBySearchText("");
+              }
+            ),
+          ),
+          onChanged: (value) => _filterListBySearchText(value),
+          onSubmitted: (value) => _filterListBySearchText(value),
+          ),
+        ),
+      );
+    
+    var favoritesSearchResults = ListView.builder(
+      physics: const AlwaysScrollableScrollPhysics(),
+      controller: _scrollController,
+      itemCount: _filteredFavoritesList.length,
+      shrinkWrap: true,
+      padding: const EdgeInsets.only(bottom: 10),
+      itemBuilder: (context, itemIdxs) {
+        if (itemIdxs < _filteredFavoritesList.length) {
+          var favoriteTiles = ListTile(
+            title: Text(_filteredFavoritesList[itemIdxs]),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () async {
+                _removeFavorite(_filteredFavoritesList[itemIdxs]);
+              },
+            )
+          );
+          return favoriteTiles;
+        }
+      },
+    );
+      
+    // TODO-TD: move to circular button hovering in bottom right corner
     var addFavButtonAppBar = <Widget>[
       IconButton(
-          icon: const Icon(
-            Icons.star,
-          ),
-          onPressed: () async {
-            var resultLabel = await _showTextInputDialog(context);
-            if (resultLabel != null) {
-              setState(() {
-                _addFavorite(resultLabel);
-              });
-            }
-          })
+        icon: const Icon(
+          Icons.star,
+        ),
+        onPressed: () async {
+          var resultLabel = await _showTextInputDialog(context);
+          if (resultLabel != null) {
+            setState(() {
+              _addFavorite(resultLabel);
+              _filterListBySearchText("");
+            });
+          }
+        }
+      )
     ];
 
     var pageLayout = Scaffold(
-        appBar: AppBar(
-            backgroundColor: black,
-            title: const Text('Favorites', style: TextStyle(color: white)),
-            actions: addFavButtonAppBar),
-        body: _buildList(),
-        backgroundColor: gray);
+      appBar: AppBar(
+          title: const Text('Favorites'),
+          actions: addFavButtonAppBar,
+        ),
+      body: Scaffold(
+        appBar: favoritesSearchBar,
+        body: favoritesSearchResults,
+      )
+    );
 
     return pageLayout;
   }
--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,60 +1,258 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:first_flight/style.dart';
-import 'package:first_flight/sett_model.dart';
 
-// Settings page
 class SettingsPage extends StatefulWidget {
+  // Settings page
   const SettingsPage({super.key});
 
   @override
-  State<SettingsPage> createState() => _SettingsState();
+  State<SettingsPage> createState() => _SettingsPageState();
 }
 
-class _SettingsState extends State<SettingsPage> {
-  late SettModel _model;
+class _SettingsPageState extends State<SettingsPage> {
+    // Search bar
+    // - Display only the search results
+
+    // - reset all settings
+    // - control location services
+    // - enable disable notifications
+    // - sensor calibration
+
+  final _textFieldController = TextEditingController();
+  final _editingController = TextEditingController();
+
+  final List<String> titles = [
+    "Email", 
+    "Language"
+  ];
+
+  List<String> filteredItems = [];
+  
+  SharedPreferences? preferences;
+  String defaultEmail = 'person@email.com';
+  String email = 'person@email.com';
+  
+  void _clearMemory(String name){
+
+    List<String>? savedData = preferences?.getStringList(name);
+
+    if (savedData == null) {
+      setState(() {});
+
+    } else {
+
+      for (final saved in savedData) {
+        savedData.remove(saved);
+      }
+
+      preferences?.setStringList(name, savedData);
+      setState(() {});
+    }
+  }
+
+  void filterSearchResults(String query) {
+    setState(() {
+      filteredItems = titles
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+  Future<void> initStorage() async {
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      _loadEmail();
+    });
+  }
 
   @override
   void initState() {
+    initStorage();
     super.initState();
   }
 
-  @override
-  void dispose() {
-    _model.dispose();
-    super.dispose();
+  void _updateEmail(name) {
+    email = name;
+    preferences?.setString("email", email);
+    setState(() {});
   }
 
-  @override
-  Widget build(BuildContext context) {
-    var widgets = Padding(
-      padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
-      child: SwitchListTile.adaptive(
-        value: _model.switchListTileValue1 ??= true,
-        onChanged: (newValue) async {
-          setState(() => _model.switchListTileValue1 = newValue);
-        },
-        title: const Text(
-          'Push Notifications',
-        ),
-        subtitle: const Text(
-          'Receive Push notifications from our application on a semi regular basis.',
-        ),
-        tileColor: Colors.white,
-        activeColor: const Color(0xFF4B39EF),
-        activeTrackColor: const Color(0x4C4B39EF),
-        contentPadding: const EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+  void _loadEmail() {
+    String? savedData = preferences?.getString('email');
+
+    if (savedData == null) {
+      preferences?.setString("email", defaultEmail);
+    } else {
+      email = savedData;
+    }
+
+    setState(() {});
+  }
+
+  void _removeEmail() {
+    email = defaultEmail;
+    preferences?.setString("email", defaultEmail);
+    setState(() {});
+  }
+
+  Future<String?> _showTextInputDialog(BuildContext context) async {
+    var dialogBox = AlertDialog(
+      title: const Text('Add email'),
+      content: TextField(
+        controller: _textFieldController,
+        decoration: const InputDecoration(hintText: "Email"),
       ),
+      actions: <Widget>[
+        ElevatedButton(
+          child: const Text("Exit"),
+          onPressed: () => Navigator.pop(context),
+        ),
+        ElevatedButton(
+          child: const Text('Add'),
+          onPressed: () => Navigator.pop(context, _textFieldController.text),
+        ),
+      ],
     );
 
-    var pageLayout = Scaffold(
-        appBar: AppBar(
-          backgroundColor: black,
-          title: const Text('Settings', style: TextStyle(color: white)),
-        ),
-        body: Column(mainAxisSize: MainAxisSize.max, children: [widgets]),
-        backgroundColor: gray);
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return dialogBox;
+        }
+      );
+  }
+  @override
+  Widget build(BuildContext context){
 
-    return pageLayout;
+    var searchBar = TextField(
+      controller: _editingController,
+      decoration: const InputDecoration(
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        labelText: "Search",
+        hintText: "Search Settings",
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0)
+          )
+        )
+      )
+    );
+
+
+    var emailTile = ListTile(
+      leading: const Icon(Icons.mail),
+      title: const Text("Email"),
+      subtitle: Text(email),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () async {
+          _removeEmail();
+        },
+      ),
+      onTap: () async {
+        var resultLabel = await _showTextInputDialog(context);
+        if (resultLabel != null) {
+          setState(() {
+            _updateEmail(resultLabel);
+            }
+          );
+        }
+      }
+    );
+
+    var clearHistoryTile = ListTile(
+      leading: const Icon(Icons.history),
+      title: const Text("Clear History"),
+      subtitle: const Text("Clear all search history"),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete_forever_sharp),
+        onPressed: () async {_clearMemory("History");},
+      )
+    );
+
+    var clearFavoritesTile = ListTile(
+      leading: const Icon(Icons.star),
+      title: const Text("Clear Favorites"),
+      subtitle: const Text("Clear all favorites"),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete_forever_sharp),
+        onPressed: () async {_clearMemory("Favorites");},
+      )
+    );
+
+    var themeConsumer = Consumer<ModelThemeProvider>(
+      builder: (context, ModelThemeProvider themeNotifier, child) {
+
+        var darkmodeTile = ListTile(
+          leading: Icon(
+            themeNotifier.isDark ? Icons.nightlight_round : Icons.wb_sunny
+          ),
+          title: Text(themeNotifier.isDark ? "Dark Mode" : "Light Mode"),
+          trailing: Switch(
+              value: themeNotifier.isDark,
+              activeColor: Colors.purple,
+              onChanged: (val) {
+                setState(() {
+                  themeNotifier.isDark = val;
+                }
+              );
+            }
+          ),
+        );
+
+        var settingsBody = SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [Text("Common")],
+                ),
+                const ListTile(
+                  title: Text("Location"),
+                  leading: Icon(Icons.pin_drop),
+                  subtitle: Text("Here"),
+                ),
+                const ListTile(
+                  title: Text("Notifications"),
+                  leading: Icon(Icons.notifications),
+                  subtitle: Text("Get notified on upcoming passes"),
+                ),
+                const ListTile(
+                  title: Text("Language"),
+                  leading: Icon(Icons.language),
+                  subtitle: Text("English"),
+                ),
+                const Divider(),
+                emailTile,
+                darkmodeTile,
+                const Divider(),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [Text("Account")],
+                ),
+                clearHistoryTile,
+                clearFavoritesTile
+              ],
+            ),
+          ),
+        );
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Settings"),
+          ),
+          body: Column(
+            children: [
+              searchBar, 
+              settingsBody
+            ]
+          )
+        );
+      }
+    );
+    return themeConsumer;       
   }
 }
