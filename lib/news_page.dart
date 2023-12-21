271827e38b4bb863453aca875f2085e6d0a7829c
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-21 16:01:46 -0500

    news_page.dart: link and format articles

--- lib/news_page.dart
+++ lib/news_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 
 class Article{
-  final String id;
+  final int id;
   final String title;
   final String url;
   final String image_url;
@@ -27,10 +28,10 @@ class Article{
     required this.summary,
   });
 
-  factory Article.fromJson(dynamic json) {
+  factory Article.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-        'id': String id,
+        'id': int id,
         'title': String title, 
         'url': String url, 
         'image_url': String image_url, 
@@ -45,44 +46,69 @@ class Article{
           news_site: news_site,
           summary: summary,
         ),
-      _ => throw const FormatException('Failed to load Orbit.'),
+      _ => throw const FormatException('Failed to load Article.'),
     };
   }
 }
 
 Future<List<Article>> querySpaceNews() async {
-  String spaceFlightNews = "http://api.spaceflightnewsapi.net/v4/articles/";
-
-  final response = await http.get(Uri.parse(spaceFlightNews));
+  // Show me everything since yesterday
+  // TODO-TD: toggle look time
+  String spaceFlightNews = "http://api.spaceflightnewsapi.net/v4/articles/?published_at_gte=";
+  final now = DateTime.now();
+  final yesterday = '${now.year.toString()}-${now.month.toString()}-${(now.day-1).toString()}';
+  final response = await http.get(Uri.parse(spaceFlightNews + yesterday));
       
   if (response.statusCode == 200) {
-    Iterable l = json.decode(response.body);
-    List<Article> articles = List<Article>.from(l.map((model) => Article.fromJson(model)));
-    return articles;
+    Map<String, dynamic> decoded = json.decode(response.body) as Map<String, dynamic>;
+    if (decoded.containsKey("results")){
+      Iterable res = decoded["results"];
+      List<Article> articles = List<Article>.from(
+        res.map((jsonArticle) => Article.fromJson(jsonArticle))
+      );
+      return articles;
 
+    } else {
+    throw Exception('No results section');
+    }
   } else {
     throw Exception('Failed to load articles');
   }
 
 }
 
+Future<void> _launchUrl(String _url) async {
+  if (!await launchUrl(Uri.parse(_url))) {
+    throw Exception('Could not launch $_url');
+  }
+}
 var newsFeedBuilder = FutureBuilder<List<Article>>(
-  future: futureArticles,
+  future: querySpaceNews(),
   builder: (context, snapshot) {
+    int ellipseStart = 200;
+
     if (snapshot.hasData) {
       List<Article> articles = snapshot.data!;
-
       return ListView.builder(
-        reverse: true,
         itemBuilder: (context, itemIdxs) {
-    
           if (itemIdxs < articles.length) {
+            Article article = articles[itemIdxs];
+            String summary = article.summary;
+
+            if (summary.length > ellipseStart){
+              summary = summary.replaceRange(
+                ellipseStart, 
+                summary.length, 
+                '...'
+              );
+            }
+
             var orbitTile = ListTile(
-              onTap: () {
-                // Open URL in browser
-              },
-              title: Text(articles[itemIdxs].title),
-              subtitle: Text('Summary: ${articles[itemIdxs].summary}'),
+              // TODO-TD: fetch and display image
+              onTap: () {_launchUrl(article.url);},
+              title: Text(article.title),
+              subtitle: Text('$summary\nSource: ${article.news_site}'),
+              // TODO-TD: link related satellites
             );
             return orbitTile;
           }
